97de9a4149566db1652e80ee8458fa98776621db
abdullah-hamdy-98 <abdo1231992260@gmail.com>
2024-10-19 22:01:21 +0300

    td

--- components/forms/empInquiry/InquiryTable.tsx
+++ components/forms/empInquiry/InquiryTable.tsx
@@ -6,11 +6,11 @@ function InquiryTable() {
                 <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                     <tr>
                         <th scope="col" className="px-6 py-3">
-                            Product name
+                            Employee Name
                         </th>
                         <th scope="col" className="px-6 py-3">
                             <div className="flex items-center">
-                                Color
+                                Job
                                 <a href="#"><svg className="w-3 h-3 ms-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
                                     <path d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z" />
                                 </svg></a>
@@ -26,7 +26,7 @@ function InquiryTable() {
                         </th>
                         <th scope="col" className="px-6 py-3">
                             <div className="flex items-center">
-                                Price
+                                Company
                                 <a href="#"><svg className="w-3 h-3 ms-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
                                     <path d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z" />
                                 </svg></a>
@@ -40,53 +40,45 @@ function InquiryTable() {
                 <tbody>
                     <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
                         <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                            Apple MacBook Pro 17"
+                            Abdullah Hamdy
                         </th>
                         <td className="px-6 py-4">
-                            Silver
+                            Frontend Dev.
                         </td>
                         <td className="px-6 py-4">
-                            Laptop
+                            White Collar
                         </td>
                         <td className="px-6 py-4">
-                            $2999
+                            Login HR
                         </td>
-                        <td className="px-6 py-4 text-right">
-                            <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-                        </td>
+
                     </tr>
                     <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
                         <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                            Microsoft Surface Pro
+                            Ibrahim Farag
                         </th>
                         <td className="px-6 py-4">
-                            White
+                            Technical Team Lead
                         </td>
                         <td className="px-6 py-4">
-                            Laptop PC
+                            White Collar
                         </td>
                         <td className="px-6 py-4">
-                            $1999
-                        </td>
-                        <td className="px-6 py-4 text-right">
-                            <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                            Login HR
                         </td>
                     </tr>
                     <tr className="bg-white dark:bg-gray-800">
                         <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                            Magic Mouse 2
+                            Muhammed Musallam
                         </th>
                         <td className="px-6 py-4">
-                            Black
+                            Senior Technical Support
                         </td>
                         <td className="px-6 py-4">
-                            Accessories
+                            White Collar
                         </td>
                         <td className="px-6 py-4">
-                            $99
-                        </td>
-                        <td className="px-6 py-4 text-right">
-                            <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                        Login HR
                         </td>
                     </tr>
                 </tbody>
